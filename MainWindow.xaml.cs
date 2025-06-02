638b2c7542a9f799bc18786e919c5677e82c1b02
unicode1 <egoraleshin2006@gmail.com>
2025-06-03 00:41:49 +0300

    вот я долбоеб 2

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace coh2ui;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void AddButton(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void EditButton(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DeleteButton(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void NineButton(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void KeysList_OnMouseDoubleClickClicked(object sender, MouseButtonEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
